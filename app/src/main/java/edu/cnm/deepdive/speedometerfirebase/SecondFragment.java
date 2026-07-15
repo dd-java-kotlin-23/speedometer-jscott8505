082940d8164a2cf54705e8d7cae0c404886868bf
jazz <jscott85@student.cnm.edu>
2026-07-15 10:55:41 -0600

    Added user submission functionality with RecyclerView for submission history display.

--- app/src/main/java/edu/cnm/deepdive/speedometerfirebase/SecondFragment.java
+++ app/src/main/java/edu/cnm/deepdive/speedometerfirebase/SecondFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import dagger.hilt.android.AndroidEntryPoint;
 import edu.cnm.deepdive.speedometerfirebase.databinding.FragmentSecondBinding;
 import edu.cnm.deepdive.speedometerfirebase.viewmodel.SecondFragmentViewModel;
@@ -23,7 +27,7 @@ public class SecondFragment extends Fragment {
   @Override
   public View onCreateView(
       @NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-     @Nullable Bundle savedInstanceState) {
+      @Nullable Bundle savedInstanceState) {
     binding = FragmentSecondBinding.inflate(inflater, container, false);
     return binding.getRoot();
   }
@@ -35,20 +39,47 @@ public class SecondFragment extends Fragment {
     secondFragmentViewModel = new ViewModelProvider(this).get(SecondFragmentViewModel.class);
 
     secondFragmentViewModel.getUser().observe(getViewLifecycleOwner(), (user) -> {
-        if (user == null) {
-          NavController navController = Navigation.findNavController(requireView());
-          if (navController.getCurrentDestination() != null
-              && navController.getCurrentDestination().getId() == R.id.SecondFragment) {
-            navController.navigate(R.id.action_SecondFragment_to_FirstFragment);
-          }
-        } else {
-          binding.loginInfo.setText(getString(R.string.logged_in_as, user.getEmail()));
+      if (user == null) {
+        NavController navController = Navigation.findNavController(requireView());
+        if (navController.getCurrentDestination() != null
+            && navController.getCurrentDestination().getId() == R.id.SecondFragment) {
+          navController.navigate(R.id.action_SecondFragment_to_FirstFragment);
         }
+      } else {
+        binding.loginInfo.setText(getString(R.string.logged_in_as, user.getEmail()));
+      }
     });
 
     binding.buttonLogout.setOnClickListener((v) -> {
       secondFragmentViewModel.signOut();
     });
+
+    secondFragmentViewModel.getAllSubmissions().observe(getViewLifecycleOwner(), (userSubmissions) -> {
+          binding.historyRecyclerView.setAdapter(new Adapter() {
+            @NonNull
+            @Override
+            public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+              View historyRow = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.activity_list_item, parent, false);
+              return new ViewHolder(historyRow){
+              };
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+              ((TextView) holder.itemView.findViewById(android.R.id.text1)).setText(userSubmissions.get(position).getSpeed());
+            }
+
+            @Override
+            public int getItemCount() {
+              return userSubmissions.size();
+            }
+          });
+        });
+
+        binding.submitButton.setOnClickListener((v) -> {
+          int speedValue = Integer.parseInt(binding.speedNumber.getText().toString());
+          secondFragmentViewModel.addSubmission(speedValue);
+        });
   }
 
   @Override
--- app/src/main/java/edu/cnm/deepdive/speedometerfirebase/viewmodel/SecondFragmentViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/speedometerfirebase/viewmodel/SecondFragmentViewModel.java
@@ -5,17 +5,23 @@ import androidx.lifecycle.ViewModel;
 import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseUser;
 import dagger.hilt.android.lifecycle.HiltViewModel;
+import edu.cnm.deepdive.speedometerfirebase.model.UserSubmission;
+import edu.cnm.deepdive.speedometerfirebase.repository.UserSubmissionRepository;
 import edu.cnm.deepdive.speedometerfirebase.service.SignInService;
 import jakarta.inject.Inject;
+import java.util.List;
 
 @HiltViewModel
 public class SecondFragmentViewModel extends ViewModel {
 
   private final SignInService signInService;
+  private final UserSubmissionRepository userSubmissionRepository;
 
   @Inject
-  SecondFragmentViewModel(SignInService signInService) {
+  SecondFragmentViewModel(SignInService signInService,
+      UserSubmissionRepository userSubmissionRepository) {
     this.signInService = signInService;
+    this.userSubmissionRepository = userSubmissionRepository;
   }
 
   public LiveData<FirebaseUser> getUser() {
@@ -24,6 +30,14 @@ public class SecondFragmentViewModel extends ViewModel {
 
   public void signOut() {
     signInService.signOut();
+  }
+
+  public void addSubmission(int speed) {
+    userSubmissionRepository.addSubmission(speed, signInService.getUser().getValue().getUid());
+  }
+
+  public LiveData<List<UserSubmission>> getAllSubmissions() {
+    return userSubmissionRepository.getAllSubmissions();
   }
 
 }
